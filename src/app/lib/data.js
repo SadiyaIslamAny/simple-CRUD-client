2132ad4f30cea62ee79e8f63d29251cb2f6af99b
SADIA <you@example.com>
2026-06-07 21:48:52 +0600

    feat: fetch and display user details by ID

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -3,3 +3,10 @@ export const getUsers = async() =>{
     const usersData = await res.json();
     return usersData
 }
+
+
+export const getUserById = async(userId) =>{
+       const res =await fetch(`http://localhost:5000/users/${userId}`);
+       const data = await res.json();
+       return data;
+}
